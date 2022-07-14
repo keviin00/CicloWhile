6edd8cce2cfbecb40eab53ab0d621076a5b16abf
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-07-14 11:15:36 -0300

    Add files via upload

---
+++ ciclo while prueba/src/test1/ciclowhile.java
@@ -0,0 +1,22 @@
+package test1;
+public class ciclowhile {
+
+
+		 public static void main(String[]args) {
+			 int contador =0;
+			 
+			 do {  
+				 System.out.print(contador + "\n");
+				 contador++;
+				 
+				 
+			 }while (contador <= 200);
+		 }
+}
+
+	
+
+
+
+
+
